ee83d215fcbd1cf0b36441045ec63017f32d2436
monicapong <111400935+monicapong@users.noreply.github.com>
2022-11-10 01:44:24 -0500

    added js code

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var apiKey = "3de7f199adb168e35cb20780e93be5af";
 var currentWeather = $('#currentWeather');
+var forecast = $('#weatherForecast');
 
 function renderCurrentWeather(city) {
     var requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
@@ -15,20 +16,58 @@ function renderCurrentWeather(city) {
 
         currentWeather.append(`
             <h2>
-                ${cityWeatherResponse.name} (${moment().format('L')}) <img src='${iconURL}'> 
+                ${cityWeatherResponse.name} (${moment().format('L')}) 
             </h2>
+            <img src='${iconURL}'>
             <p>Temperature: ${cityWeatherResponse.main.temp} °F</p>
             <p>Wind: ${cityWeatherResponse.wind.speed} MPH</p>
             <p>Humidity: ${cityWeatherResponse.main.humidity}\%</p>
         `);
+
+        $(currentWeather).css('text-align', 'center');
     });
 };
 
+function renderForecast(city) {
+    requestURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
+
+    $.get(requestURL).then(function(forecastResponse){
+        weatherForecast = forecastResponse.list;
+        forecast.empty();
+
+        $.each(weatherForecast, function(i) {
+            if (!weatherForecast[i].dt_txt.includes("12:00:00")) {
+                return;
+            }
+            let forecastDate = new Date(weatherForecast[i].dt*1000);
+            let weatherIcon = `https://openweathermap.org/img/wn/${weatherForecast[i].weather[0].icon}.png`;
+
+            forecast.append(`
+            <div class="col-md">
+                <div class="card text-white">
+                    <div class="card-body">
+                        <h4>${forecastDate.getMonth()+1}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4>
+                        <img src=${weatherIcon} alt="Icon">
+                        <p>Temp: ${weatherForecast[i].main.temp} &#176;C</p>
+                        <p>Wind: ${weatherForecast[i].wind.speed} m/s</p>
+                        <p>Humidity: ${weatherForecast[i].main.humidity}%</p>
+                    </div>
+                </div>
+            </div>
+            `)
+
+            $(forecast).css('text-align', 'center');
+        })
+    })
+};
+
 renderCurrentWeather('Miami');
+renderForecast('Miami');
 
 $("#searchBtn").on("click", function(event) {
     event.preventDefault();
 
     var city = $('#cityInput').val().trim();
     renderCurrentWeather(city);
+    renderForecast(city);
 });
